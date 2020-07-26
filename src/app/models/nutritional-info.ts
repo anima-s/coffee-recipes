8c1544044f57d257197227410ab3bab98a57c73b
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:17:09 +0530

    Create nutritional-info.ts

---
+++ src/app/models/nutritional-info.ts
@@ -0,0 +1,9 @@
+export class NutritionalInfo{
+    public nutritionMapping: any = {
+        //Nutrion id vs coffee items list
+        //Assume that nutrition id is same as coffee id for now
+        // 1: [1, 2, 3],
+        // 2: [4, 5, 6]
+    }
+}
+
