77435ab22a14fe65a8414b059d65fa7ce4878ef8
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:07:18 +0530

    Update add-item.component.ts

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { CartService } from '../service/cart.service';
+import { CoffeeDetailsService } from '../service/coffee-details.service';
+import { CartItem } from '../models/CartItem';
 
 @Component({
   selector: 'app-add-item',
@@ -7,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() recipe:string
+  @Input() item:string
+  constructor(public popoverController: PopoverController,
+    private cartService: CartService,
+    private coffeeDetailsService: CoffeeDetailsService) { 
+  }
 
-  ngOnInit() {}
+  cart_items: CartItem[];
+
+  ngOnInit() {
+    // this.cart_items = this.cartService.getUserCart(1);
+  }
+
+ closePopover() {
+   this.popoverController.dismiss();
+   
+  }
+
+  addItemToCart() {
+    this.popoverController.dismiss();
+    this.cartService.addCustomEntryToCart(this.recipe,this.item)
+    
+    // console.log(this.cartService.getUserCart(1))
+    
+  }
+
 
 }
