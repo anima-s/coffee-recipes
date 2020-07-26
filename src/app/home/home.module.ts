2369358a259d1a10fe54078258d86ecbb29a1239
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:13:20 +0530

    Update home.module.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+
+import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CoffeeListComponent } from '../coffee-list/coffee-list.component';
 
 
 @NgModule({
@@ -12,8 +16,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+   
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, NavbarComponent, CoffeeListComponent]
 })
 export class HomePageModule {}
