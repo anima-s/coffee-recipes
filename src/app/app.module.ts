6a894cdd17734e70e4b783699e44bccc543c065f
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:25:15 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { LoadingController } from '@ionic/angular';
+import { CoffeeTypeArray } from './models/coffee-type';
+
+// plugins
 import { SQLite } from '@ionic-native/sqlite/ngx';
- 
 import { HttpClientModule } from '@angular/common/http';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LoadingController,
+    CoffeeTypeArray,
     SQLite,
-    SQLitePorter
+    SQLitePorter,
+    {
+      provide: [RouteReuseStrategy], useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
