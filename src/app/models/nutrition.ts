14b41dbd527614713842dcb1c38643eba75c1f66
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:16:47 +0530

    Create nutrition.ts

---
+++ src/app/models/nutrition.ts
@@ -0,0 +1,8 @@
+export class Nutrition{
+    public id: any;
+    public energy: string;
+    public fat: string;
+    public carbs: string;
+    public protein: string;
+    public sodium: string;
+}
