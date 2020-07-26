f8b064afa4085daa09ea158b683e528e53db6883
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:15:23 +0530

    Create CartKeyMap.ts

---
+++ src/app/models/CartKeyMap.ts
@@ -0,0 +1,10 @@
+export class CartKeyMap {
+    recipe_id: number;
+    item_ids: number[];
+
+    // constructor(recipe, item) {
+    //     this.recipe_id = recipe;
+    //     this.item_ids.push(item);
+    // }
+}
+
