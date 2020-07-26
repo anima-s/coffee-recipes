5d9cc9304b5e3c00874d3564ba75da1471a2f29d
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:24:04 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,26 +3,38 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'details',
+    children: [
+      {
+        path: ':id',
+        children: [
+          {
+          path:'',
+          loadChildren: () => import('./coffee-details/coffee-details.module').then(m => m.CoffeeDetailsPageModule)
+          },
+          {
+            path: 'nutritional-info',
+            loadChildren: () => import('./nutritional-info/nutritional-info.module').then(m => m.NutritionalInfoPageModule)
+          }
+        ]
+      },
+     
+    ]
+  },
+  {
     path: 'cart',
     loadChildren: () => import('./cart/cart.module').then( m => m.CartPageModule)
-  },
-  {
-    path: 'coffee-details',
-    loadChildren: () => import('./coffee-details/coffee-details.module').then( m => m.CoffeeDetailsPageModule)
-  },
-  {
-    path: 'nutritional-info',
-    loadChildren: () => import('./nutritional-info/nutritional-info.module').then( m => m.NutritionalInfoPageModule)
-  },
+  }
+ 
 ];
 
 @NgModule({
@@ -31,4 +43,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
