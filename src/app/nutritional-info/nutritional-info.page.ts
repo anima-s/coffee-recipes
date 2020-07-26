7d3959f03d3ff71cc67e74a508fc966382d2c8c3
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:20:01 +0530

    Update nutritional-info.page.ts

--- src/app/nutritional-info/nutritional-info.page.ts
+++ src/app/nutritional-info/nutritional-info.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Nutrition } from '../models/nutrition';
+import { ActivatedRoute } from '@angular/router';
+import { NutritionService } from '../service/nutrition.service';
+import { CoffeeDetailsService } from '../service/coffee-details.service';
 
 @Component({
   selector: 'app-nutritional-info',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nutritional-info.page.scss'],
 })
 export class NutritionalInfoPage implements OnInit {
+  @Input() nutrition: Nutrition;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private nutritionService: NutritionService,
+    private coffeeDetailsService: CoffeeDetailsService
+  ) { }
+  
+  nutritional_info: Nutrition;
+  id: string;
+  method: string[];
 
   ngOnInit() {
+
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+
+    this.nutritional_info = this.nutritionService.getNutritionalInfo(this.id);
+    this.method = this.coffeeDetailsService.getMethod(this.id);
+
   }
+
 
 }
