d51e839b29790081bb21279676efa299c0ba81c8
anima153 <anima.sharma15@gmail.com>
2020-07-26 22:09:18 +0530

    Update cart.page.ts

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CartService } from '../service/cart.service';
+import { Coffee } from '../models/coffee';
+import { ActivatedRoute } from '@angular/router';
+import { CoffeeDetailsService } from '../service/coffee-details.service';
+import { PopoverController } from '@ionic/angular';
+import { AddItemComponent } from '../add-item/add-item.component'
+import { CartItem } from '../models/CartItem';
+
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
 })
-export class CartPage implements OnInit {
 
-  constructor() { }
+export class CartPage implements OnInit {
+  public coffee: Coffee;
+  public cart_items: CartItem[]=[];
+
+  constructor(private cartService: CartService, private route: ActivatedRoute,
+    private coffeeDetailsService: CoffeeDetailsService,
+    public popover: PopoverController) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      // console.log(params)
+    });
+
+    this.cart_items = this.cartService.getUserCart(1);
+  }
+
+  ngAfterContentChecked() {
+    this.refreshCart();
+  }
+
+ addNewItemToCart() {
+   this.showPopover();
+  }
+
+  removeItemFromCart(ele: Element, ent: CartItem) {
+    this.cartService.removeItemFromCart(ent.recipe, ent.sub_item)
+    this.refreshCart()
+  }
+
+  refreshCart() {
+    this.cart_items = this.cartService.getUserCart(1);
+  }
+
+  async showPopover() {
+    const popover = await this.popover.create({
+      component: AddItemComponent,
+      cssClass: 'my-custom-class',
+      // event: ev,
+      backdropDismiss: false,
+      translucent: true
+    });
+
+    popover.onDidDismiss()
+      .then((result) => {
+        if (result.data !== undefined) {
+          // console.log(result.data.recipe);
+          // console.log(result.data.item);
+          return result.data;
+        }
+      });
+
+    return await popover.present();
   }
 
 }
